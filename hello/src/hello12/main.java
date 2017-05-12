64f44205a36f0f4693773d2f39f41de3027ec395
bit-user <bit-user@bit>
2017-05-12 14:40:34 +0900

    12341234231423

--- hello/src/hello12/main.java
+++ hello/src/hello12/main.java
@@ -1,48 +1,10 @@
 package hello12;
 
-// // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
-import java.net.URI;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 public class main 
 {
     public static void main(String[] args) 
     {
-        HttpClient httpclient = HttpClients.createDefault();
-
-        try
-        {
-            URIBuilder builder = new URIBuilder("https://westus.api.cognitive.microsoft.com/face/v1.0/persongroups/bit-jjy-test-001/persons");
-
-
-            URI uri = builder.build();
-            HttpGet request = new HttpGet(uri);
-            request.setHeader("Ocp-Apim-Subscription-Key", "{subscription key}");
-
-
-            // Request body
-            /*StringEntity reqEntity = new StringEntity("{body}");
-            request.setEntity(reqEntity);*/
-
-            HttpResponse response = httpclient.execute(request);
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) 
-            {
-                System.out.println(EntityUtils.toString(entity));
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
     }
 }
 
